584de2e4730552ea69d3767a521824ef5cee2ca0
bulofkgames <leodiyu@gmail.com>
2026-01-11 14:15:18 -0300

    Update ator.js

--- js/ator.js
+++ js/ator.js
@@ -1,60 +1,37 @@
-//Código do ator
 let xAtor = 85;
 let yAtor = 366;
-let colisao = false;
 let meusPontos = 0;
 
-function mostraAtor(){
+function mostraAtor() {
     image(imagemDoAtor, xAtor, yAtor, 30, 30);
 }
 
-function movimentaAtor(){
-    if(keyIsDown(UP_ARROW)){
-        yAtor -= 3;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        if(PodeSeMover()){
-            yAtor += 3;
-        }
-    }
+function movimentaAtor() {
+    if (keyIsDown(UP_ARROW)) yAtor -= 3;
+    if (keyIsDown(DOWN_ARROW) && yAtor < 366) yAtor += 3;
 }
 
-function verificaColisao(){
-    for(let i=0; i < imagemCarros.length; i++){
-        colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
-        if(colisao){
-            voltaAtorPosicaoInicial();
+function verificaColisao() {
+    for (let i = 0; i < imagemCarros.length; i++) {
+        if (collideRectCircle(xCarros[i], yCarros[i], 50, 40, xAtor, yAtor, 15)) {
+            yAtor = 366;
             somDaColisao.play();
-            if(pontoMaiorQueZero()){
-                meusPontos -= 1;
-            }
+            if (meusPontos > 0) meusPontos--;
         }
     }
 }
 
-function voltaAtorPosicaoInicial(){
-    yAtor = 366;
-}
-
-function incluiPontos(){
+function incluiPontos() {
     textAlign(CENTER);
     textSize(25);
-    fill(color(255, 240, 60));
-    text(meusPontos, width/5, 27);
+    fill(255, 240, 60);
+    text(meusPontos, width / 5, 27);
 }
 
-function marcaPonto(){
-    if(yAtor < 15){
-        meusPontos +=1;
+function marcaPonto() {
+    if (yAtor < 15) {
+        meusPontos++;
         somDoPonto.play();
-        voltaAtorPosicaoInicial();
+        yAtor = 366;
     }
-}
-
-function pontoMaiorQueZero(){
-    return meusPontos > 0;
-}
-
-function PodeSeMover(){
-    return yAtor < 366;
 }
