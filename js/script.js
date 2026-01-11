53a2b0b3e86cfbbba7c0b1b7e250a7b398e77a92
bulofkgames <leodiyu@gmail.com>
2026-01-11 14:15:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 function setup() {
     createCanvas(500, 400);
     frameRate(60);
-
-    // Inicia a trilha sonora
-    if (somDaTrilha) {
-        somDaTrilha.setVolume(0.4);
-        somDaTrilha.loop();
-    }
 }
 
 function draw() {
     background(imagemDaEstrada);
 
-    // Ator
     mostraAtor();
     movimentaAtor();
 
-    // Carros
     mostraCarro();
     movimentaCarro();
     voltaPosicaoInicialCarro();
 
-    // Colisão e pontuação
     verificaColisao();
     incluiPontos();
     marcaPonto();
+}
+
+function mousePressed() {
+    userStartAudio();
+    if (!somDaTrilha.isPlaying()) {
+        somDaTrilha.setVolume(0.4);
+        somDaTrilha.loop();
+    }
 }
