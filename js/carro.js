655c7ea615c1a3d0006ff7a99337a503a34a6abe
bulofkgames <leodiyu@gmail.com>
2026-01-11 14:23:35 -0300

    Update carro.js

--- js/carro.js
+++ js/carro.js
@@ -1,6 +1,12 @@
-let yCarros = [40, 96, 150, 210, 270, 318];
-let xCarros = [600, 600, 600, 600, 600, 600];
-let velocidadeCarros = [2, 5, 3.5, 5, 2.3, 3.1];
+let yCarros = [60, 120, 180, 240, 300, 360];
+let xCarros = [];
+let velocidadeBase = [2, 3, 2.5, 3.5, 2.2, 3];
+
+function iniciarCarros() {
+    xCarros = Array(6).fill(width);
+}
+
+iniciarCarros();
 
 function mostraCarro() {
     for (let i = 0; i < imagemCarros.length; i++) {
@@ -10,14 +16,14 @@ function mostraCarro() {
 
 function movimentaCarro() {
     for (let i = 0; i < imagemCarros.length; i++) {
-        xCarros[i] -= velocidadeCarros[i];
+        xCarros[i] -= velocidadeBase[i] + nivel * 0.6;
     }
 }
 
 function voltaPosicaoInicialCarro() {
     for (let i = 0; i < imagemCarros.length; i++) {
-        if (xCarros[i] < -50) {
-            xCarros[i] = width;
+        if (xCarros[i] < -60) {
+            xCarros[i] = width + random(100, 400);
         }
     }
 }
