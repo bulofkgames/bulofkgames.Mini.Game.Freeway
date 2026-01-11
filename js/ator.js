9c5d47796f49ec4296aea57f123a73f6ac4e90eb
bulofkgames <leodiyu@gmail.com>
2026-01-11 14:23:14 -0300

    Update ator.js

--- js/ator.js
+++ js/ator.js
@@ -1,6 +1,7 @@
 let xAtor = 85;
 let yAtor = 366;
 let meusPontos = 0;
+let recorde = localStorage.getItem("recordeFreeway") || 0;
 
 function mostraAtor() {
     image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -8,30 +9,37 @@ function mostraAtor() {
 
 function movimentaAtor() {
     if (keyIsDown(UP_ARROW)) yAtor -= 3;
-    if (keyIsDown(DOWN_ARROW) && yAtor < 366) yAtor += 3;
+    if (keyIsDown(DOWN_ARROW) && yAtor < height - 34) yAtor += 3;
 }
 
 function verificaColisao() {
     for (let i = 0; i < imagemCarros.length; i++) {
         if (collideRectCircle(xCarros[i], yCarros[i], 50, 40, xAtor, yAtor, 15)) {
-            yAtor = 366;
             somDaColisao.play();
-            if (meusPontos > 0) meusPontos--;
+            jogoAtivo = false;
+            salvarRecorde();
         }
     }
 }
 
 function incluiPontos() {
-    textAlign(CENTER);
-    textSize(25);
     fill(255, 240, 60);
-    text(meusPontos, width / 5, 27);
+    textAlign(CENTER);
+    text(`Pontos: ${meusPontos}`, width / 2, 30);
+    text(`Recorde: ${recorde}`, width / 2, 60);
 }
 
 function marcaPonto() {
-    if (yAtor < 15) {
+    if (yAtor < 10) {
         meusPontos++;
         somDoPonto.play();
-        yAtor = 366;
+        yAtor = height - 34;
+    }
+}
+
+function salvarRecorde() {
+    if (meusPontos > recorde) {
+        recorde = meusPontos;
+        localStorage.setItem("recordeFreeway", recorde);
     }
 }
