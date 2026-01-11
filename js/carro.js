8b3937955c72034f56664bf7ee420f3e24e265d1
bulofkgames <leodiyu@gmail.com>
2026-01-11 14:15:38 -0300

    Update carro.js

--- js/carro.js
+++ js/carro.js
@@ -1,34 +1,23 @@
-// Posições iniciais dos carros
 let yCarros = [40, 96, 150, 210, 270, 318];
 let xCarros = [600, 600, 600, 600, 600, 600];
-
-// Velocidade de cada carro
 let velocidadeCarros = [2, 5, 3.5, 5, 2.3, 3.1];
 
-// Mostra os carros na tela
 function mostraCarro() {
     for (let i = 0; i < imagemCarros.length; i++) {
         image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
     }
 }
 
-// Move os carros para a esquerda
 function movimentaCarro() {
     for (let i = 0; i < imagemCarros.length; i++) {
         xCarros[i] -= velocidadeCarros[i];
     }
 }
 
-// Reposiciona o carro quando sai da tela
 function voltaPosicaoInicialCarro() {
     for (let i = 0; i < imagemCarros.length; i++) {
-        if (passouTodaTela(xCarros[i])) {
-            xCarros[i] = width; // melhor que 600 (responsivo)
+        if (xCarros[i] < -50) {
+            xCarros[i] = width;
         }
     }
-}
-
-// Verifica se o carro saiu totalmente da tela
-function passouTodaTela(xCarro) {
-    return xCarro < -50;
 }
