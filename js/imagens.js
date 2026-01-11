aa353fedf802e978cb11b121a844d6404238178d
bulofkgames <leodiyu@gmail.com>
2026-01-11 14:14:58 -0300

    Update imagens.js

--- js/imagens.js
+++ js/imagens.js
@@ -1,25 +1,25 @@
-//Imagens e sons do jogo
-
-//Imagens do Jogo
+// Imagens
 let imagemDaEstrada;
 let imagemDoAtor;
-let imagemCarro;
-let imagemCarro2;
-let imagemCarro3;
+let imagemCarros = [];
 
-//Sons do jogo
+// Sons
 let somDaTrilha;
 let somDaColisao;
 let somDoPonto;
 
+function preload() {
+    imagemDaEstrada = loadImage("imagens/estrada.png");
+    imagemDoAtor = loadImage("imagens/ator-1.png");
 
-function preLoad(){
-    imagemDaEstrada = loadImage("../imagens/estrada.png");
-    imagemDoAtor = loadImage("../imagens/ator-1.png");
-    imagemCarro = loadImage("../imagens/carro-1.png");
-    imagemCarro2 = loadImage("../imagens/carro-2.png");
-    imagemCarro3 = loadImage("../imagens/carro-3.png");
-    let imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
+    imagemCarros = [
+        loadImage("imagens/carro-1.png"),
+        loadImage("imagens/carro-2.png"),
+        loadImage("imagens/carro-3.png"),
+        loadImage("imagens/carro-1.png"),
+        loadImage("imagens/carro-2.png"),
+        loadImage("imagens/carro-3.png")
+    ];
 
     somDaTrilha = loadSound("sons/trilha.mp3");
     somDaColisao = loadSound("sons/colidiu.mp3");
